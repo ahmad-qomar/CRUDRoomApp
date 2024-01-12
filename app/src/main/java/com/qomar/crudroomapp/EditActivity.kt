23f5dafe86e1da061b125bf5fbec4149fd658e74
Ahmad Qomarudin <pusattiksisma@gmail.com>
2024-01-12 14:00:58 +0700

    Update bug
    Update bug

--- app/src/main/java/com/qomar/crudroomapp/EditActivity.kt
+++ app/src/main/java/com/qomar/crudroomapp/EditActivity.kt
@@ -12,54 +12,63 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-private infix fun <ConstraintLayout> ViewBinding.setContentView(root: ConstraintLayout): ActivityEditBinding {
-
+private infix fun <ConstraintLayout> ViewBinding.setContentView(root: ConstraintLayout) {
 }
 
 class EditActivity : AppCompatActivity() {
-    val db by lazy { NoteDB(this) }
+    val db by lazy { NoteDB(this)}
     private var noteId: Int = 0
     private lateinit var binding : ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityEditBinding.inflate(layoutInflater) setContentView(binding.root)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupView()
         setupListener()
     }
     fun setupView(){
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        val intentType = intent.getIntExtra("intent_type", 0) when(intentType){
+        val intentType = intent.getIntExtra("intent_type", 0)
+        when(intentType){
             Constant.TYPE_CREATE -> {
-                binding.buttonUpdate.visibility = View.GONE  }
+                binding.buttonUpdate.visibility = View.GONE
+            }
             Constant.TYPE_READ -> {
-                binding.buttonSave.visibility = View.GONE  binding.buttonUpdate.visibility = View.GONE  getNote()
+                binding.buttonSave.visibility = View.GONE
+                binding.buttonUpdate.visibility = View.GONE
+                getNote()
             }
             Constant.TYPE_UPDATE -> {
-                binding.buttonSave.visibility = View.GONE  getNote()
+                binding.buttonSave.visibility = View.GONE
+                getNote()
             }
         }
     }
     private fun setupListener() {
         binding.buttonSave.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {  db.run {
-                noteDao().addNote(
-                    Note(0, binding.editTitle.text.toString(), binding.editNote.text.toString())
-                    )
-            }
+            CoroutineScope(Dispatchers.IO).launch {
+                db.noteDao().addNote(
+                    Note(0, binding.editTitle.text.toString(),
+                        binding.editNote.text.toString())
+                )
                 finish()
             }
         }
         binding.buttonUpdate.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {  db.noteDao().updateNote(
-                Note(noteId, binding.editTitle.text.toString(),  binding.editNote.text.toString())
-            )
+            CoroutineScope(Dispatchers.IO).launch {
+                db.noteDao().updateNote(
+                    Note(noteId, binding.editTitle.text.toString(),
+                        binding.editNote.text.toString())
+                )
                 finish()
             }
         }
     }
     fun getNote(){
-        noteId = intent.getIntExtra("intent_id", 0)  CoroutineScope(Dispatchers.IO).launch {
-            val notes = db.noteDao().getNote(noteId)[0]  binding.editTitle.setText(notes.title)
+        noteId = intent.getIntExtra("intent_id", 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db.noteDao().getNote(noteId)[0]
+            binding.editTitle.setText(notes.title)
             binding.editNote.setText(notes.note)
         }
     }
--- app/src/main/java/com/qomar/crudroomapp/room/NoteDB.kt
+++ app/src/main/java/com/qomar/crudroomapp/room/NoteDB.kt
@@ -9,15 +9,22 @@ import androidx.room.RoomDatabase
     entities = [Note::class],
     version = 1
 )
-abstract class NoteDB: RoomDatabase() {
+abstract class NoteDB : RoomDatabase() {
+
     abstract fun noteDao(): NoteDao
+
     companion object {
-        @Volatile private var instance: NoteDB? = null
+        @Volatile
+        private var instance: NoteDB? = null
         private val LOCK = Any()
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){  instance ?: buildDatabase(context).also{
-            instance = it
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
         }
-        }
-        private fun buildDatabase(context: Context)= Room.databaseBuilder(context.applicationContext, NoteDB::class.java,"note12345.db").build()
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, NoteDB::class.java, "note12345.db").build()
     }
 }
